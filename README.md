f51812f653548792ad8f9b4c080c4d0261838496
Szilárd Huber <szilard.huber@gmail.com>
2021-10-29 20:01:32 +0200

    Use argument as input and try out converting string to numbers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Very first idea: It must be possible with linux tools like sed, awk, grep
+### Very first idea: It must be possible with linux tools like sed, awk, grep
 
 ```ggrep -P '(?=.*?76)(?=.*73)(?=.*?19)(?=.*?88)(?=.*?78)^.*$' 10m-v2.txt | wc -l ```
 
@@ -6,8 +6,16 @@ Time for this: `3.01s user 0.04s system 99% cpu 3.082 total`
 
 The other problem is that we would need to generate all possible combinations of 2,3,4 occurance numbers.
 
-# Go implementation
+### Go implementation
 
 Naive one: Don't convert strings to numbers, just do a plain split and count the occurances.
 
 Time: `3.88s user 0.19s system 106% cpu 3.811 total` But this at least seems to return all needed results.
+
+
+### Optimization ideas
+
+1. Sort each line upfront. As there is around an hour preparation time before the show, we can do some preparation. Naive implementation: 
+`while read -r line;do echo $line | tr ' ' '\n' | sort -n | paste -sd' ' -  ;done < 10k-v2.txt > 10k_sorted.txt`
+
+Takes around 48s on my laptop, which means that for the 10m rows it would be 1000 times that much which is too long.
--- lottery.go
+++ lottery.go
@@ -2,9 +2,12 @@ package main
 
 import (
     "fmt"
+    "log"
     "bufio"
 	"os"
     "strings"
+    "time"
+    "strconv"
 )
 
 func contains(a []string, i string) bool {
@@ -16,32 +19,86 @@ func contains(a []string, i string) bool {
     return false
 }
 
-func processLine(line string) uint {
+func containsUint(a []uint, i uint) bool {
+    for _, item := range a {
+        if item == i {
+            return true
+        }
+    }
+    return false
+}
+
+func split(line string) []uint {
+    items := strings.Split(line, " ")
+    ret := make([]uint, len(items))
+    for i, v := range items {
+        u64, _ := strconv.ParseUint(v, 10, 16)
+        ret[i] = uint(u64)
+    }
+    return ret
+}
+
+func processLine(line string, winningNumbers []string) uint {
     var ret uint = 0
     items := strings.Split(line, " ")
-    winner := strings.Split("76 73 19 88 78", " ")
-    //winner := strings.Split("49 35 37 34 68", " ")
     for _, item := range items {
-        if contains(winner, item) {
+        if contains(winningNumbers, item) {
             ret += 1
         }
     }
     return ret
 }
 
-func main() {
-    results := make(map[uint]int32)
-    //path := "./test.txt"
-    path := "./10m-v2.txt"
-	file, _ := os.Open(path)
-    defer file.Close()
-	/* handle error */
-    scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-        result := processLine(scanner.Text())
-        if result > 1 {
-            results[result] += 1
+func processLineUint(line string, winningNumbers []uint) uint {
+    var ret uint = 0
+    items := split(line)
+    for _, item := range items {
+        if containsUint(winningNumbers, item) {
+            ret += 1
         }
-	}
-    fmt.Printf("%+v", results)
+    }
+    return ret
+}
+
+func displayOutput(results map[uint]int32, t time.Duration) {
+    if os.Getenv("DEBUG") == "1" {
+        fmt.Printf("%d %d %d %d (Time taken: %s)\n", results[2], results[3], results[4], results[5], t)
+    } else {
+        fmt.Printf("%d %d %d %d\n", results[2], results[3], results[4], results[5])
+    }
+}
+
+func main()  {
+    if len(os.Args) < 2 {
+        log.Fatalf("Usage: %s input_file_name", os.Args[0]);
+    }
+
+    path := os.Args[1]
+	file, err := os.Open(path)
+    if err != nil {
+        log.Fatalf("Can't open file: %s", path)
+    }
+    defer file.Close()
+
+    fmt.Println("READY")
+    stdinScanner := bufio.NewScanner(os.Stdin)
+    for stdinScanner.Scan() {
+        winningNumbers := strings.Split(stdinScanner.Text(), " ")
+        //winningNumbers := split(stdinScanner.Text())
+        if len(winningNumbers) != 5 {
+            break
+        }
+        start := time.Now()
+        scanner := bufio.NewScanner(file)
+        results := make(map[uint]int32)
+        for scanner.Scan() {
+            result := processLine(scanner.Text(), winningNumbers)
+            if result > 1 {
+                results[result] += 1
+            }
+        }
+        displayOutput(results, time.Since(start))
+        file.Seek(0, 0)
+    }
+
 }
