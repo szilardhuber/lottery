bb56e65354e8a572fd596125b9e78601cdb016a1
Szilárd Huber <szilard.huber@gmail.com>
2021-10-30 15:12:45 +0200

    Optimize memory usage

--- lottery.go
+++ lottery.go
@@ -7,9 +7,51 @@ import (
 	"os"
     "strings"
     "time"
+    "strconv"
 )
 
-func contains(a []string, i string) bool {
+func split(line string) ([5]uint8, error) {
+    items := strings.Split(line, " ")
+    values := make(map[uint8]bool)
+    var ret [5]uint8
+    for _, v := range items {
+        u64, err := strconv.ParseUint(v, 10, 8)
+        if err != nil || u64 < 1 || u64 > 90{
+            return ret, fmt.Errorf("Invalid input, numbers should be in the 0,90 range: %s in: %s", v, line)
+        }
+        if values[uint8(u64)] {
+            return ret, fmt.Errorf("Duplicate number: %s in: %s", v, line)
+        }
+        values[uint8(u64)] = true
+    }
+
+    if len(values) != 5 {
+        return ret, fmt.Errorf("Invalid numbers. They should contain exactly 5 numbers in the 0,90 range: %s", line)
+    }
+
+    i := 0
+    for v := range values {
+        ret[i] = v
+        i++
+    }
+
+    if len(ret) < 5 {
+        return ret, fmt.Errorf("Not enough valid numbers: %s", line)
+    }
+    return ret, nil
+}
+
+func processLineUint(items [5]uint8, winningNumbers [5]uint8) uint8 {
+    var ret uint8
+    for _, item := range items {
+        if containsUint(winningNumbers, item) {
+            ret++
+        }
+    }
+    return ret
+}
+
+func containsUint(a [5]uint8, i uint8) bool {
     for _, item := range a {
         if item == i {
             return true
@@ -18,23 +60,41 @@ func contains(a []string, i string) bool {
     return false
 }
 
-func processLine(line string, winningNumbers []string) uint {
-    var ret uint = 0
-    items := strings.Split(line, " ")
-    for _, item := range items {
-        if contains(winningNumbers, item) {
-            ret += 1
-        }
-    }
-    return ret
+func showDebug() bool {
+    return os.Getenv("DEBUG") == "1"
 }
 
-func displayOutput(results map[uint]int32, t time.Duration) {
-    if os.Getenv("DEBUG") == "1" {
+func displayOutput(results map[uint8]int32, t time.Duration) {
+    if showDebug() {
         fmt.Printf("%d %d %d %d (Time taken: %s)\n", results[2], results[3], results[4], results[5], t)
     } else {
         fmt.Printf("%d %d %d %d\n", results[2], results[3], results[4], results[5])
     }
+}
+
+func processInput(path string) [][5]uint8 {
+    startPrepare := time.Now()
+	file, err := os.Open(path)
+    if err != nil {
+        log.Fatalf("Can't open file: %s", path)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    numbers := make([][5]uint8, 0, 10000000)
+    for scanner.Scan() {
+        n, err := split(scanner.Text())
+        if err == nil {
+            numbers = append(numbers, n)
+        } else if showDebug() {
+            log.Println(err)
+        }
+    }
+
+    if showDebug() {
+        log.Printf("Finished processing input file in %s", time.Since(startPrepare))
+    }
+    return numbers
 }
 
 func main()  {
@@ -42,32 +102,25 @@ func main()  {
         log.Fatalf("Usage: %s input_file_name", os.Args[0]);
     }
 
-    path := os.Args[1]
-	file, err := os.Open(path)
-    if err != nil {
-        log.Fatalf("Can't open file: %s", path)
-    }
-    defer file.Close()
+    numbers := processInput(os.Args[1])
 
     fmt.Println("READY")
     stdinScanner := bufio.NewScanner(os.Stdin)
     for stdinScanner.Scan() {
-        winningNumbers := strings.Split(stdinScanner.Text(), " ")
-        if len(winningNumbers) != 5 {
+        winningNumbers, err := split(stdinScanner.Text())
+        if err != nil || len(winningNumbers) != 5 {
+            log.Print("No valid input provided. Exiting.")
             break
         }
         start := time.Now()
-        scanner := bufio.NewScanner(file)
-        results := make(map[uint]int32)
-        for scanner.Scan() {
-            result := processLine(scanner.Text(), winningNumbers)
+        results := make(map[uint8]int32)
+        for _, number := range numbers {
+            result := processLineUint(number, winningNumbers)
             if result > 1 {
-                results[result] += 1
+                results[result]++
             }
         }
         displayOutput(results, time.Since(start))
-        file.Seek(0, 0)
     }
 
 }
-
